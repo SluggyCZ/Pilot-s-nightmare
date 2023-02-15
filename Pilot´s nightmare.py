05c3fbad593965e625282df2516a8af3a37b2dc4
Tomáš Celba <celbat22@spstrutnov.com>
2023-02-15 20:15:19 +0100

    Jakési ovládání
    Zatím nefunguje tak jak má

--- Pilot´s nightmare.py
+++ Pilot´s nightmare.py
@@ -8,13 +8,17 @@ ROZLISENI_Y = 1020
 
 
 FPS = 60
-velikostx = 150
-velikosty = 100
-pozicex = (ROZLISENI_X - velikostx) / 2
-pozicey = (ROZLISENI_Y - velikosty) / 2
+velikost_x = 150
+velikost_y = 100
+pozice_x = (ROZLISENI_X - velikost_x) / 2
+pozice_y = (ROZLISENI_Y - velikost_y) / 2
+rychlost = 3
+smer_x = -3
+smer_y = 0
 nebe = (0, 220, 255)
+
 letadlo = pygame.image.load('letadlo.png')
-letadlo = pygame.transform.scale(letadlo, (velikostx, velikosty))
+letadlo = pygame.transform.scale(letadlo, (velikost_x, velikost_y))
 pozadi = pygame.image.load('pozadi.png')
 pozadi = pygame.transform.scale(pozadi, (ROZLISENI_X, ROZLISENI_Y))
 
@@ -27,12 +31,48 @@ while True:
         if udalost.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+    
     klavesy = pygame.key.get_pressed()
     
+    if klavesy[pygame.K_UP]:
+        if smer_x > 0:
+            smer_y += 0.25
+        elif smer_x < 0:
+            smer_y -= 0.25
+        
+    if klavesy[pygame.K_DOWN]:
+        if smer_x > 0:
+            smer_y -= 0.25
+        elif smer_x < 0:
+            smer_y += 0.25
+    
+    
+    pozice_x += smer_x
+    pozice_y += smer_y
+    
+   
+    
+    if smer_x < -3:
+        smer_x = -3
+    if smer_x > 3:
+        smer_x = 3
+    if smer_y < -3:
+        smer_y = -3
+    if smer_y > 3:
+        smer_y = 3
+    
+    if pozice_x > ROZLISENI_X - velikost_x:
+        pozice_x = 0
+    if pozice_y > ROZLISENI_Y - velikost_y:
+        pozice_y = ROZLISENI_Y - velikost_y
+    if pozice_x < 0:
+        pozice_x = ROZLISENI_X - velikost_x
+    if pozice_y < 0:
+        pozice_y = 0
     
     
     okno.blit(pozadi, (0, 0))
-    okno.blit(letadlo, (pozicex, pozicey))
+    okno.blit(letadlo, (pozice_x, pozice_y))
     
     
     pygame.display.update()
