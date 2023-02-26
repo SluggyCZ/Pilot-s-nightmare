b609dc62ae8220378aa353f283c499329e4b5278
Tomáš Celba <celbat22@spstrutnov.com>
2023-02-26 15:22:13 +0100

    Letadlo se naklání při pohybu do stran
    Zrušení pozadí kvůli lagům :(

--- Pilot´s nightmare.py
+++ Pilot´s nightmare.py
@@ -1,7 +1,7 @@
 import sys
 import pygame
-import math
 import random
+
 pygame.init()
 
 
@@ -26,9 +26,10 @@ letadlo = pygame.image.load('vrtadlo.png')
 letadlo = pygame.transform.scale(letadlo, (velikost_x, velikost_y))
 pozadi = pygame.image.load('pozadi.png')
 pozadi = pygame.transform.scale(pozadi, (ROZLISENI_X, ROZLISENI_Y))
-mrak = pygame.image.load("mrak.png")
-mrak = pygame.transform.scale(mrak, (random.randint(50,150), random.randint(50,70)))
 
+letadlo_l = pygame.transform.rotate(letadlo, 15)
+letadlo_p = pygame.transform.rotate(letadlo, -15)
+letadlo_n = letadlo
 hodiny = pygame.time.Clock()
 okno = pygame.display.set_mode((ROZLISENI_X, ROZLISENI_Y))
 pygame.display.set_caption("Pilot´s nightmare")
@@ -43,20 +44,20 @@ while True:
     
     
     if klavesy[pygame.K_UP]:
-        smer_y -= 0.25
-    
+        smer_y -= 0.5
+        letadlo = letadlo_n
     
     if klavesy[pygame.K_DOWN]:
-        smer_y += 0.25
-        
+        smer_y += 0.5
+        letadlo = letadlo_n
     
     if klavesy[pygame.K_LEFT]:
-        smer_x -= 0.25
-        
+        smer_x -= 0.5
+        letadlo = letadlo_l
         
     if klavesy[pygame.K_RIGHT]:
-        smer_x += 0.25
-        
+        smer_x += 0.5
+        letadlo = letadlo_p
     
     
     
@@ -64,7 +65,6 @@ while True:
     
     pozice_x += smer_x
     pozice_y += smer_y
-    mr1_x += 5
     if smer_x < -3:
         smer_x = -3
     if smer_x > 3:
@@ -74,19 +74,19 @@ while True:
     if smer_y > 3:
         smer_y = 3
     
+    
+    
     if pozice_x > ROZLISENI_X - velikost_x:
-        pozice_x = 0
+        pozice_x = ROZLISENI_X - velikost_x
     if pozice_y > ROZLISENI_Y - velikost_y:
         pozice_y = ROZLISENI_Y - velikost_y
     if pozice_x < 0:
-        pozice_x = ROZLISENI_X - velikost_x
+        pozice_x = 0
     if pozice_y < 0:
         pozice_y = 0
     
-    
-    okno.blit(pozadi, (0, 0))
+    okno.fill(nebe)
     okno.blit(letadlo, (pozice_x, pozice_y))
-    okno.blit(mrak, (mr1_x,mr1_y))
     
     pygame.display.update()
     hodiny.tick(FPS)
